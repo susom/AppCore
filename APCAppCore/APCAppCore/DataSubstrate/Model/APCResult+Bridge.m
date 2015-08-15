c2e2153a97f8a7fa95f0274cb1f17b52c71b9145
Justin Warmkessel <justin.w@ymedialabs.com>
2015-08-14 17:34:06 -0700

    MHC-85 Turn off debugger logging and also check if file url exists before attempting an upload.

--- APCAppCore/APCAppCore/DataSubstrate/Model/APCResult+Bridge.m
+++ APCAppCore/APCAppCore/DataSubstrate/Model/APCResult+Bridge.m
@@ -61,33 +61,38 @@
     {
         if (self.archiveFilename.length > 0) {
 
-            NSString *pathBeingUploaded = self.archiveURL.absoluteString;
-            APCLogFilenameBeingUploaded (pathBeingUploaded);
+            NSString*   pathBeingUploaded   = self.archiveURL.absoluteString;
+            BOOL        fileExists          = [[NSFileManager defaultManager] fileExistsAtPath:pathBeingUploaded];
+            
+            if (fileExists)
+            {
+                APCLogFilenameBeingUploaded (pathBeingUploaded);
 
-            [SBBComponent(SBBUploadManager) uploadFileToBridge:self.archiveURL contentType:@"application/zip" completion:^(NSError *error) {
-                
-                if (error) {
-                    APCLogError2(error);
-                } else {
-                    APCLogEventWithData(kNetworkEvent, (@{@"event_detail":[NSString stringWithFormat:@"Uploaded Task: %@    RunID: %@", self.taskID, self.taskRunID]}));
+                [SBBComponent(SBBUploadManager) uploadFileToBridge:self.archiveURL contentType:@"application/zip" completion:^(NSError *error) {
                     
-                    self.uploaded = @(YES);
-                    NSError * saveError;
-                    [self saveToPersistentStore:&saveError];
-                    
-                    //Delete archiveURLs
-                    NSString * path = [[self.archiveURL path] stringByDeletingLastPathComponent];
-                    NSError * deleteError;
-                    if (![[NSFileManager defaultManager] removeItemAtPath:path error:&deleteError]) {
-                        APCLogError2(deleteError);
+                    if (error) {
+                        APCLogError2(error);
+                    } else {
+                        APCLogEventWithData(kNetworkEvent, (@{@"event_detail":[NSString stringWithFormat:@"Uploaded Task: %@    RunID: %@", self.taskID, self.taskRunID]}));
+                        
+                        self.uploaded = @(YES);
+                        NSError * saveError;
+                        [self saveToPersistentStore:&saveError];
+                        
+                        //Delete archiveURLs
+                        NSString * path = [[self.archiveURL path] stringByDeletingLastPathComponent];
+                        NSError * deleteError;
+                        if (![[NSFileManager defaultManager] removeItemAtPath:path error:&deleteError]) {
+                            APCLogError2(deleteError);
+                        }
+                        APCLogError2 (saveError);
                     }
-                    APCLogError2 (saveError);
-                }
-                
-                if (completionBlock) {
-                    completionBlock(error);
-                }
-            }];
+                    
+                    if (completionBlock) {
+                        completionBlock(error);
+                    }
+                }];
+            }
         }
         else
         {
