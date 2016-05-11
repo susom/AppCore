54a72470a608de2312fe32ef56f40f3d9f134d9a
Paweł Kowalczyk <pawel.kowalczyk@lifemap-solutions.com>
2016-05-11 16:10:26 +0200

    MHC-201 - New Intro Screens
    Add `APCStudyOverviewCollectionViewController.h` to public headers.

--- APCAppCore/APCAppCore/APCAppCore.h
+++ APCAppCore/APCAppCore/APCAppCore.h
@@ -111,6 +111,7 @@ FOUNDATION_EXPORT const unsigned char APCAppCoreVersionString[];
 #import <APCAppCore/APCUserInfoViewController.h>
 #import <APCAppCore/APCIntroVideoViewController.h>
 #import <APCAppCore/APCStudyOverviewViewController.h>
+#import <APCAppCore/APCStudyOverviewCollectionViewController.h>
 #import <APCAppCore/APCStudyDetailsViewController.h>
 #import <APCAppCore/APCSignInViewController.h>
 #import <APCAppCore/APCForgotPasswordViewController.h>
