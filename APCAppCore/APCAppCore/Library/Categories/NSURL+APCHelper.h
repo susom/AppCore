897dcd1da26dc0cb7ec3e04d784fe8f36745dbad
Paweł Kowalczyk <pawel.kowalczyk@codete.com>
2018-09-21 08:46:14 +0200

    GP-171 Replace deprecated UIWebView in APCWebViewController

---
+++ APCAppCore/APCAppCore/Library/Categories/NSURL+APCHelper.h
@@ -0,0 +1,15 @@
+//
+//  NSURL+APCHelper.h
+//  APCAppCore
+//
+//  Created by Paweł Kowalczyk on 21.09.2018.
+//  Copyright © 2018 Apple, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (APCHelper)
+
++ (NSURL *)randomBaseURL;
+
+@end
